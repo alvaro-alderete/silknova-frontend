2691b8c5d059b087f20c4062b1b9292b3fa985ba
Alvaro Roldan Alderete <alvaroalderete77@gmail.com>
2026-04-21 19:21:41 -0300

    se crea componente decategorias

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import ProductGrid from "../components/ProductGrid.jsx";
 import PromoBanner from "../components/PromoBanner.jsx";
+import CategoryFilter from "../components/CategoryFilter.jsx";
 
 function Home() {
   return (
     <>
       <PromoBanner />
       <ProductGrid />
+      <CategoryFilter />
     </>
   );
 }
